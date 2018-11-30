ddda89d5ade0cdc33fbdb98bd4ff3bb72c9d88ef
martynovp <martynovp@gmail.com>
2018-11-30 14:01:02 +0300

    EDX-1357 change url

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     packages=find_packages(),
     description='Mass e-mail utils for PLP and SSO',
-    url='https://github.com/miptliot/npoed-massmail',
+    url='https://github.com/miptliot/tp-massmail',
     author='TP',
     install_requires=['django>=2',
                       'emails',
